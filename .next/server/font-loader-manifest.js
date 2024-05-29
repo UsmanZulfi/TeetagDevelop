e104ce4a52d6e28114d95ed3331ee303cbeb0ebc
UsmanZulfiqar <apple@UsmanMac-2017.local>
2024-05-29 12:54:55 +0500

    initial commit

--- .next/server/font-loader-manifest.js
+++
@@ -1,9 +0,0 @@
-self.__FONT_LOADER_MANIFEST={
-  "pages": {
-    "/_app": [
-      "static/media/3b401319a061ecfc.p.woff2",
-      "static/media/4007bd53a5e80126.p.woff2"
-    ]
-  },
-  "app": {}
-}
